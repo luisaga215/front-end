23d06c422b2c9a9848542629599dbe77b3880afc
luisaga215 <luis.aga215@gmail.com>
2018-11-06 17:20:51 -0600

    JS del supervisor con link tag

---
+++ supervisor.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    var employees = [
+        'Luis',
+        'Ruben',
+        'Denisse'
+    ];
+
+    var ul = document.createElement('ul');
+
+    document.getElementById("div_activos").appendChild(ul);
+
+    employees.forEach(function (employees) {
+        var a = document.createElement('a');
+        a.setAttribute('href',"");
+        var li = document.createElement('li');
+        ul.appendChild(li);
+        li.appendChild(a);
+        a.innerHTML += employees;
+    });
+});
