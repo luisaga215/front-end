f8119568b1224f188b22e840437cc194c6ad2ad9
luisaga215 <luis.aga215@gmail.com>
2018-11-06 20:59:48 -0600

    search bar incluida

--- supervisor.js
+++ supervisor.js
@@ -1,39 +1,34 @@
 $(document).ready(function () {
-
+    //Variable lista de empleados activos
     var employees = [
-        'Luis',
+        'Luis Glz',
         'Ruben',
         'Denisse',
-        'Luis',
-        'Ruben',
-        'Denisse',
-        'Luis',
-        'Ruben',
-        'Denisse',
-        'Luis',
-        'Ruben',
-        'Denisse',
-        'Luis',
-        'Ruben',
-        'Denisse',
-        'Luis',
-        'Ruben',
-        'Denisse',
-        'Luis',
-        'Ruben',
-        'Denisse'
+        'Rodrigo',
+        'Alex',
+        'Nestor',
+        'Missael',
+        'Fernanda',
+        'Luis C.',
+        'Luis Hdz',
+        'Vania',
+        'JuanPi',
+        'Diego',
+        'Christofer'
     ];
 
     var ul = document.createElement('ul');
-
+    ul.setAttribute('id', "myUL");
     document.getElementById("div_activos").appendChild(ul);
-
+    //Funcion para agregar empleados activos a la lista
     employees.forEach(function (employees) {
         var a = document.createElement('a');
         a.setAttribute('href', "#");
         var li = document.createElement('li');
+        li.setAttribute('class', "the_li");
         ul.appendChild(li);
         li.appendChild(a);
         a.innerHTML += employees;
     });
+
 });
