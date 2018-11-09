a2de01c0a26de88a4bb5d25aa7c19d83a2d5a3fb
luisaga215 <luis.aga215@gmail.com>
2018-11-09 13:14:52 -0600

    update admin y carpetas

---
+++ admin.js
@@ -0,0 +1,9 @@
+function ToggleDiv() {
+    var x = document.getElementById("Agregar");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
