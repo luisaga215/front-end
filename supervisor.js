a92464e55588b596609afbf57abc8b28bc0419dd
luisaga215 <luis.aga215@gmail.com>
2018-11-09 18:09:47 -0600

    Puntos en mapa implementados

--- supervisor.js
+++ supervisor.js
@@ -1,45 +1,52 @@
 //Variable lista de empleados activos
 var employees = [
-        ['Luis Glz',100,150],
-        ['Ruben',100,150],
-        ['Denisse',100,150],
-        ['Rodrigo',100,150],
-        ['Alex',100,150],
-        ['Nestor',100,150],
-        ['Missael',100,150],
-        ['Fernanda',100,150],
-        ['Luis C.',100,150],
-        ['Luis Hdz',100,150],
-        ['Vania',100,150],
-        ['JuanPi',100,150],
-        ['Diego',100,150],
-        ['Christofer',100,150]
+        'Luis Glz',
+        'Ruben',
+        'Denisse',
+        'Rodrigo',
+        'Alex',
+        'Nestor',
+        'Missael',
+        'Fernanda',
+        'Luis C.',
+        'Luis Hdz',
+        'Vania',
+        'JuanPi',
+        'Diego',
+        'Christofer'
     ];
-
-//Generar lista de empleados activos
-function generateList() {
-    //Generar lista
-    var ul = document.createElement('ul');
-    ul.setAttribute('id', "myUL");
-    document.getElementById("div_activos").appendChild(ul);
-
-    //Funcion para agregar empleados activos a la lista
-    employees.forEach(function (employees) {
-        var a = document.createElement('a');
-        a.setAttribute('href', "#");
-        var li = document.createElement('li');
-        li.setAttribute('class', "the_li");
-        ul.appendChild(li);
-        li.appendChild(a);
-        a.innerHTML += employees;
-    });
-
-    //Funcion para cambiar el color del content en la lista al hacer click
-    $("ul a").click(function () {
-        $("ul a").removeClass('active');
-        $(this).addClass('active');
-    });
-};
+var pointsX = [
+        1,
+        120,
+        110,
+        105,
+        80,
+        75,
+        120,
+        40,
+        30,
+        250,
+        500,
+        360,
+        420,
+        480
+    ];
+var pointsY = [
+        1,
+        120,
+        110,
+        105,
+        80,
+        250,
+        330,
+        360,
+        210,
+        270,
+        200,
+        100,
+        150,
+        168
+    ];
 
 //Puntos en el mapa
 var Marker = function () {
@@ -51,10 +58,61 @@ var Marker = function () {
     this.YPos = 0;
 }
 var Markers = new Array();
+var counterA = 1;
+var activePoint;
+//Generar lista de empleados activos
+function generateList() {
+    //Generar lista
+    var ul = document.createElement('ul');
+    ul.setAttribute('id', "myUL");
+    document.getElementById("div_activos").appendChild(ul);
+
+    //Funcion para agregar empleados activos a la lista
+    employees.forEach(function (employees) {
+        var a = document.createElement('a');
+        a.setAttribute('href', "#");
+        a.setAttribute('id', counterA);
+        counterA++;
+        var li = document.createElement('li');
+        li.setAttribute('class', "the_li");
+        ul.appendChild(li);
+        li.appendChild(a);
+        a.innerHTML += employees;
+    });
+
+    //Funcion para cambiar el color del content en la lista al hacer click, almacena el numero donde se hizo click para cambiar la imagen en mapa
+    $("ul a").click(function () {
+        $("ul a").removeClass('active');
+        $(this).addClass('active');
+        activePoint = $(this).attr('id');
+    });
+
+};
+
+
 //Generar puntos en el mapa
 function generatePoints() {
 
+    var i;
+    var canvs = document.getElementById("supCanva");
+    var ctx = canvs.getContext("2d");
+    var markImg = new Image();
+    var markImg2 = new Image();
+    markImg.src = "./assets/marker_red.png";
+    markImg2.src = "./assets/marker_green.png";
+
+    for (i = 1; i <= employees.length; i++) {
+        if (i == activePoint) {
+            ctx.drawImage(markImg2, pointsX[i-1], pointsY[i-1], 20, 20);
+        }
+        if (i != activePoint) {
+            ctx.drawImage(markImg, pointsX[i-1], pointsY[i-1], 20, 20);
+        }
+    }
+
 }
+
+setInterval(generatePoints, 1000);
 
 //Barra de busqueda
 function filterList() {
